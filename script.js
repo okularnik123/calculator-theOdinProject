f3bc62e59d34ffbb1ded3a3317f20dd5a488e82b
okularnik123 <okularnik7a@gmail.com>
2024-01-28 11:55:11 +0100

    Created variables for all buttons Created button hover effect

---
+++ script.js
@@ -0,0 +1,9 @@
+const clearButton = document.querySelector("#clearbtn");
+const deleteButton = document.querySelector("#deletebtn");
+const devideButton = document.querySelector("#devidebtn");
+const multiplyButton = document.querySelector("#multiplybtn");
+const addButton = document.querySelector("#addbtn");
+const substractButton = document.querySelector("#subtractbtn");
+const equalsButton = document.querySelector("#equalsbtn");
+
+clearButton.addEventListener(() => {});
